5b303548b40a3c0ca619e0f484ba41e8d2dc002a
JackTurner98 <jackturner@cmail.carleton.ca>
2018-11-17 17:18:52 -0500

    Communication Pi, socket initialization

--- CommPi.py
+++ CommPi.py
@@ -1,5 +1,34 @@
 import string, socket, sys, time, serial;
-output = " "
+
 ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
-while True:
-    print(ser.readLine())
+
+serverIP = "0.0.0.0"
+appIP = "0.0.0.0"
+
+toServerPort = "0"
+fromServerPort = "0"
+toAppPort = "0"
+fromAppPort = "0"
+
+toServerAddress = (serverIP, toServerPort)
+fromServerAddress = (serverIP, fromServerPort)
+toAppAddress = (appIP, toAppPort)
+fromAppAddress = (appIP, fromAppPort)
+
+toServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+fromServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+toAppSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+fromAppSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+toServerSocket.bind(toServerAddress)
+fromServerSocket.bind(fromServerAddress)
+toAppSocket.bind(toAppAddress)
+fromAppSocket.bind(fromAppAddress)
+
+if __name__ == "__main__":
+    while True:
+        fromServer = fromServerSocket.recvfrom(2048)
+        fromApp = fromAppSocket.recvfrom(2048)
+        print(ser.readLine())
+
+
