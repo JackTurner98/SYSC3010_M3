7fe0eb56182ae214852897729c1d3ae7e43c4c3e
JackTurner98 <jackturner@cmail.carleton.ca>
2018-12-01 20:16:22 -0500

    Everything communicates, cleaned up the code a bit and added comments. Database is partly integrated, I think.

--- CommPi.py
+++ CommPi.py
@@ -1,76 +1,88 @@
 import string, socket, sys, time, json, select, serial
 
-ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
+#   SYSC 3010 - Automated Aquarium Monitoring Communication Pi
+#   Version: December 1st 2018
+#   Author: John Turner
 
-thisIP = "169.254.0.2"
-serverIP = "169.254.187.140"
-appIP = "169.254.187.140"
+ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)  # Initialize the serial port
 
+# The IPs for communication, * Make sure both devices are using the same IP types (IPv4 and Eth/WLAN)
+thisIP = "169.254.0.2"  # IP of this Device
+serverIP = "169.254.187.140"  # IP of the Server Pi
+appIP = "169.254.187.140"  # IP of the App
 
-toServerPort = 5000
+toServerPort = 5000  # Assign ports for communication
 fromServerPort = 5001
 toAppPort = 5002
 fromAppPort = 5003
 
-toServerAddress = (serverIP, toServerPort)
+toServerAddress = (serverIP, toServerPort)  # Generate address using IPs and Ports
 fromServerAddress = (thisIP, fromServerPort)
 toAppAddress = (appIP, toAppPort)
 fromAppAddress = (thisIP, fromAppPort)
 
-toServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+toServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create UDP sockets that accept IPs
 fromServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 toAppSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 fromAppSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-fromServerSocket.bind(fromServerAddress)
+fromServerSocket.bind(fromServerAddress)  # Bind the receiving sockets to an Address
 fromAppSocket.bind(fromAppAddress)
 
-fromSockets = [fromServerSocket, fromAppSocket]
-appNeeds = False
+fromSockets = [fromServerSocket, fromAppSocket]  # Array of receiving sockets that will be checked until one receives
 
+Running = True  # While set the system will run
+
+# Requests and receives current sensor data from the Arduino
 def getCurrentState():
-
-    ser.write('1')
-    read = ser.read_until('}')
-    sensorData = json.loads(read)
+    read = '{'  # Detecting a JSON packet cuts off the first { so it is added back.
+    ser.write('p')  # Send the request to the Arduino for new data
+    ser.read_until('{')  # Wait until a JSON packet is received
+    read += ser.read_until('}')  # Read until the end of the JSON packet
+    sensorData = json.loads(read)  # Load the received packet into a dictionary
     return sensorData
 
-def sendCurrentState(toSend):
-    packet = {"data":0, "temp":toSend['temp'], "level":toSend['level'], "overflow":toSend['overflow']}
-    toServerSocket.sendto(json.dumps(packet).encode(), toServerAddress)
 
+# Sending Feed instruction to the Arduino
+def startFeed():
+    ser.write('f')  # Send feed instruction to Arduino
+
+# Sends the current sensor data to the Server/Processing Pi
+def sendCurrentState(toSend):
+    packet = {"data": 0, "temp": toSend['temp'], "level": toSend['level'], "overflow": toSend['overflow']}
+    toServerSocket.sendto(json.dumps(packet).encode(), toServerAddress)  # Sends repacked data to the server Pi
+
+# Sends Processed Data to the App
 def sendProcessedData(toSend):
-    toAppSocket.sendto(toSend, toAppAddress)
+    toAppSocket.sendto(toSend, toAppAddress)  # Sends a Packet to the app address
+
 
 if __name__ == "__main__":
-    while True:
-        print("\nwaiting for instruction")
-        socketCheck,_,_ = select.select(fromSockets, [], [])
-        for socket in socketCheck:
-            receivedData, add = socket.recvfrom(2048)
-            fromData = json.loads((receivedData).decode())
-            print("Received Instruction: " + str(fromData))
-            if(int(fromData["data"]) == 5): #Instruction: Get current sensor data, send to server
-                sendCurrentState(getCurrentState())
-                print("got current state, sending back")
-                receivedProcessedData, addSecond = fromServerSocket.recvfrom(2048)
-                fromServerSecond = json.loads(receivedProcessedData.decode())
-                print("Received processed Data: " + str(fromServerSecond))
-                instr = int(fromServerSecond["data"])
-                if( instr == 6 or instr == 2 or instr == 3 or instr == 4): #Instruction: Received processed data, send to app
-                    sendProcessedData(receivedProcessedData)
-                    print("sending processed data to app now")
+    while Running:
+        socketCheck, _, _ = select.select(fromSockets, [], [])  # Checks if App or Server has requested something
+
+        for socket in socketCheck:  # For each of the sockets that received data specific operations will be done
+            receivedData, add = socket.recvfrom(2048)  # Check was data was sent to this Pi
+            fromData = json.loads(receivedData.decode())  # Load the JSON into a dictionary
+
+            if (int(fromData["data"]) == 5):  # Instruction: Get current sensor data, send to server
+                sendCurrentState(getCurrentState())  # Get and then send the current sensor data to the Server
+
+                receivedProcessedData, addSecond = fromServerSocket.recvfrom(2048)  # Receive the processed data
+                fromServerSecond = json.loads(receivedProcessedData.decode())  # Load the received data into dictionary
+                instr = int(fromServerSecond["data"])  # Variable to check the current instruction
+
+                if (instr >= 2 and instr <= 7 and not instr == 5):  # Instruction: Received processed data, send to app
+                    if (instr == 7):  # If the feeder needs to be turned on
+                        startFeed()  # Send Feed Instruction
+                    sendProcessedData(receivedProcessedData)  # Send the processed data to the App
+
+            elif (int(fromData["data"]) == 8):  # Instruction: Received target date, send to server for query
+                toServerSocket.sendto(receivedData, toServerAddress)  # Send the data that was received
+                receivedPastData, addSecond = fromServerSocket.recvfrom(2048)  # Receive the queried data
+                sendProcessedData(receivedPastData)  # Send the queried data to the app
 
 
-            elif(int(fromData["data"]) == 8): #Instruction: Received target date, send to server for query
-                toServerSocket.sendto(receivedData, toServerAddress)
-                print("Sending Date query")
-                receivedPastData, addSecond = fromServerSocket.recvfrom(2048)
-                pastData = json.loads(receivedPastData.decode())
-                print("Received past data: " + str(pastData))
-                #if(int(pastData["data"]) == 9): #Instruction: Received queried data, send to App
-                sendProcessedData(receivedPastData)
-                print("Sending past data to app now")
 
 
 
--- appSend.py
+++ appSend.py
@@ -1,8 +1,12 @@
 import socket, json
 
-thisIP = "169.254.187.140"
+# SYSC 3010 - Automated Aquarium Monitoring App Receiving/Sending
+# Version: December 1st 2018
+# Author: John Turner
 
-toCommIP = "169.254.0.2"
+thisIP = "169.254.187.140"  # The IP of this device
+
+toCommIP = "169.254.0.2"    # The IP of the Communication Pi
 toCommPort = 5003
 toCommAddress = (toCommIP, toCommPort)
 toCommSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,7 +20,7 @@ fromCommSocket.bind(fromCommAddress)
 
 if __name__ == "__main__":
     #print("Sending Test packet")
-    #toSend = {"data":8, "TEST":4}
+    toSend = {"data":8, "date":(2018, 12, 2)}
     #toCommSocket.sendto(json.dumps(toSend).encode(), toCommAddress)
     while True:
         received, add = fromCommSocket.recvfrom(2048)
